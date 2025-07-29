28e9ed0085ac5c0f3a012a7e966393c8ba5653b2
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-07-29 14:21:47 +0100

    replace anvil helper script

--- script/shared/deployment-parameters/DeploymentParamsSelector.sol
+++ script/shared/deployment-parameters/DeploymentParamsSelector.sol
@@ -11,6 +11,10 @@ library DeploymentParamsSelector {
         if (chainId == 31_337) return DeploymentParamsAnvil.getDeploymentParams();
         if (chainId == 43_113 || chainId == 43_114) return DeploymentParamsAvalanche.getDeploymentParams();
         if (chainId == 84_532) return DeploymentParamsBase.getDeploymentParams();
+
+        // For custom anvil chain ids used in demo scripts
+        if (chainId > 0) return DeploymentParamsAnvil.getDeploymentParams();
+
         revert("Unsupported chain");
     }
 }
