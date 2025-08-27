9296ab00e4340c30792462aee94d1af30825aa5d
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-08-27 09:50:43 +0100

    update parameter name used in getSwapFeeAmount function

--- src/interfaces/IRouter.sol
+++ src/interfaces/IRouter.sol
@@ -101,7 +101,7 @@ interface IRouter {
 
     // -------- View Functions --------
 
-    function getSwapFeeAmount(uint256 totalFee) external view returns (uint256);
+    function getSwapFeeAmount(uint256 totalFees) external view returns (uint256);
     function getRequestId(TransferParams memory p) external view returns (bytes32);
     function getChainID() external view returns (uint256);
     function getBlsValidator() external view returns (address);
