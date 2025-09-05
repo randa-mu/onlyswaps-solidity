fb9d8f32536a05d402413a90494bfa6f7e9ab2d2
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-09-05 14:41:58 +0100

    update docs

--- script/shared/JsonUtils.sol
+++ script/shared/JsonUtils.sol
@@ -75,7 +75,9 @@ contract JsonUtils is Script {
 
         // Attempt to read the file using vm.readFile(filePath)
         // This will throw an error if the file doesn't exist, which we catch below
-        try vm.readFile(_constructJsonFilePath((filePath))) returns (string memory /* content */ ) {}
+        try vm.readFile(_constructJsonFilePath((filePath))) returns (string memory /* content */ ) {
+            // We can store the file contents here in case we need it later.
+        }
         catch {
             fileExists = false;
         }
