6f5360c123d2a4a73c7aaf8792b9c994a0eed55c
Patrick McClurg <patrick.mcclurg@protocol.ai>
2025-09-09 22:41:26 +0200

    feat: enable second local anvil chain for testing
    I've been trying to auto-build docker images for local testing and keep
    tripping into this. Chainlist doesn't show any other chains with 31338,
    so hopefully it stays free!

--- script/shared/deployment-parameters/DeploymentParamsSelector.sol
+++ script/shared/deployment-parameters/DeploymentParamsSelector.sol
@@ -8,7 +8,7 @@ import "./DeploymentParamsBase.sol";
 library DeploymentParamsSelector {
     function getDeploymentParams(uint256 chainId) internal pure returns (DeploymentParameters memory) {
         // Local Anvil deployment
-        if (chainId == 31_337) return DeploymentParamsAnvil.getDeploymentParams();
+        if (chainId == 31_337 || chainId == 31_338) return DeploymentParamsAnvil.getDeploymentParams();
         if (chainId == 43_113 || chainId == 43_114) return DeploymentParamsAvalanche.getDeploymentParams();
         if (chainId == 84_532) return DeploymentParamsBase.getDeploymentParams();
         revert("Unsupported chain");
