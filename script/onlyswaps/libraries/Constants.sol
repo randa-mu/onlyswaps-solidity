39d37d2e155e26a490ef428a7f867f5dcadbf6a4
Nemitari Ajienka <nemitariajienka@gmail.com>
2025-11-16 23:46:47 +0000

    deploy to base sepolia and filecoin testnet

--- script/onlyswaps/libraries/Constants.sol
+++ script/onlyswaps/libraries/Constants.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8;
 
 library Constants {
-    bytes32 constant SALT = bytes32(uint256(1234567));
+    bytes32 constant SALT = bytes32(uint256(12));
 
     string constant SIGNATURE_SCHEME_ID = "BN254";
 
